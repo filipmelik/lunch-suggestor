b60188f059ab74d6a22332db78223f3142408c7c
Filip Melik <filip.melik@heureka.cz>
2017-07-21 13:50:36 +0200

    init commit

---
+++ lunch-suggestor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPTDIR="`dirname $0`"
+IFS=$'\n' read -d '' -r -a hospody < "`dirname $0`/restaurants.txt"
+
+RANDOM=$$$(date +%s)
+
+vybranahospoda=${hospody[$RANDOM % ${#hospody[@]} ]}
+
+echo Je čas na oběd, Zahoďte lopaty a běžte žrát do $vybranahospoda.
